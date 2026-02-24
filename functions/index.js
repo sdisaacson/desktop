dd96995bcd3e19a65334923fbc90fa3d26f8c760
scott <scott@sisaacson.io>
2026-02-24 03:33:43 +0000

    i hate fgunctions

--- functions/index.js
+++ functions/index.js
@@ -24,9 +24,6 @@ import * as logger from "firebase-functions/logger";
 setGlobalOptions({ maxInstances: 10 });
 
 // Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// //https://f//irebase.google.com/docs/functions/get-started
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+
